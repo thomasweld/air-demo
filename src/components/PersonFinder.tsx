b3707fcd7aa666a4a158e828ae4ea56a6aca9ede
Thomas Weld <thomas.weld@pluto.tv>
2020-11-07 17:09:36 -0500

    virtualized the list with react-window

--- src/components/PersonFinder.tsx
+++ src/components/PersonFinder.tsx
@@ -1,4 +1,8 @@
 import React, { ReactElement, useState, useEffect } from 'react';
+import {
+  ListChildComponentProps,
+  VariableSizeList as List,
+} from 'react-window';
 
 import peopleData from '../assets/peopleData.json';
 import { Person } from '../types';
@@ -7,6 +11,15 @@ import PersonDetailsBox from './PersonDetailsBox';
 const PersonFinder = (): ReactElement => {
   const [filteredList, setFilteredList] = useState<Person[]>([]);
   const [searchString, setSearchString] = useState('');
+
+  const Row = ({ index, style, data }: ListChildComponentProps) => (
+    <PersonDetailsBox {...data[index]} key={data[index].id} style={style} />
+  );
+
+  const getItemSize = () => {
+    const w = window.innerWidth;
+    return w < 500 ? 300 : 96;
+  };
 
   const handleSearchStringChange = (event: {
     target: { value: React.SetStateAction<string> };
@@ -36,11 +49,16 @@ const PersonFinder = (): ReactElement => {
         onChange={handleSearchStringChange}
       />
 
-      <ul>
-        {filteredList.map((person) => {
-          return <PersonDetailsBox {...person} key={person.id} />;
-        })}
-      </ul>
+      <List
+        height={600}
+        itemCount={filteredList.length}
+        itemSize={getItemSize}
+        estimatedItemSize={96}
+        width={'100%'}
+        itemData={filteredList}
+      >
+        {Row}
+      </List>
     </div>
   );
 };
